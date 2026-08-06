edd0ad81a14a0d1c19c51e7b73ffb41df6a18a3a
Azhar Ali <azharbinanwar@gmail.com>
2026-08-06 15:09:12 +0500

    make-a-dmg 1.0.1

--- Formula/make-a-dmg.rb
+++ Formula/make-a-dmg.rb
@@ -1,8 +1,8 @@
 class MakeADmg < Formula
   desc "Turn a macOS .app into a polished, drag-to-install .dmg"
   homepage "https://github.com/azharbinanwar/make-a-dmg"
-  url "https://github.com/azharbinanwar/make-a-dmg/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "fb1e18b8ac7f93c5ddaa2a6fe3d4512d959b1e135c23576aeb6357930bf1c8e4"
+  url "https://github.com/azharbinanwar/make-a-dmg/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6d30ee8d71cede95ff126876a14399c7a2191d328ee1ca6d86c5b7abae3d61ef"
   license "MIT"
   head "https://github.com/azharbinanwar/make-a-dmg.git", branch: "main"
 
